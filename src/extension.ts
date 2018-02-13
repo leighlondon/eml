84616dc148406fd739431c730e4112620e82bb4c
Leigh London <leigh.00.london@gmail.com>
2018-02-14 10:35:58 +1100

    ext: batch commands

--- src/extension.ts
+++ src/extension.ts
@@ -5,11 +5,13 @@ import * as vscode from "vscode";
 export function activate(context: vscode.ExtensionContext) {
   console.log('Congratulations, your extension "eml" is now active! test');
 
-  let disposable = vscode.commands.registerCommand("extension.sayHello", () => {
-    vscode.window.showInformationMessage("Hello World!");
-  });
+  let commands: Array<vscode.Disposable> = [];
 
-  context.subscriptions.push(disposable);
+  commands.push(vscode.commands.registerCommand("extension.sayHello", () => {
+    vscode.window.showInformationMessage("Hello World!");
+  }));
+
+  context.subscriptions.push(...commands);
 }
 
 export function deactivate() {}
