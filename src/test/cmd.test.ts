6388d0445492fe80c6b065c7562639396f3f7737
Leigh London <leigh.00.london@gmail.com>
2018-03-07 20:27:15 +1100

    test: move a test

---
+++ src/test/cmd.test.ts
@@ -0,0 +1,11 @@
+import * as assert from "assert";
+
+import helloWorld from "../cmd/hello";
+
+describe("cmd", () => {
+  context("helloWorld", () => {
+    it("is a function", () => {
+      assert.equal(typeof helloWorld, "function");
+    });
+  });
+});
