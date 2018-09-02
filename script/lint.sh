b7b78e7c31c45342c04be7c4ff9886167d9d46ec
Leigh London <leigh.00.london@gmail.com>
2018-09-02 16:28:08 +1000

    script: enhanced lint

--- script/lint.sh
+++ script/lint.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 set -eu
+echo '--- linting source'
 prettier --write 'src/**' 'syntaxes/*.tmLanguage.json' '*.js' 'package.json'
+echo '--- linting scripts'
+shellcheck script/*
